091f04066c00133c26eb8313c1cde24a235b6999
Sejfudin <sejfudindu@maestralsolutions.com>
2022-12-29 22:16:45 +0100

    toolkit for registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/header/Header";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Home from './pages/Home';
+import Header from './components/header/Header';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 function App() {
   return (
     <>
-    <Header />
+    <Provider store={store}>
+      <Header />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
+      {/* <ToastContainer /> */}
+      </Provider>
     </>
   );
 }
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -8,9 +8,71 @@ import {
   Grid,
   TextField,
   Typography,
-} from '@mui/material'
+} from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import { register, reset } from '../featured/auth/authSlice';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: '',
+    address: '',
+    zipcode: '',
+    discount: false,
+  });
+
+  const { username, email, password, password2, address, zipcode, discount } = formData;
+
+   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+   const { user, isLoading, isError, isSuccess, message } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate('/');
+    }
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  const onChange = (e: any) => {
+    if (e.target.name === 'discount') {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.checked,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.name]: e.target.value,
+      }));
+    }
+  };
+
+  const onSubmit = (e: any) => {
+    if (password !== password2) {
+       toast.error('Passwords do not match');
+    } else {
+      const userData = {
+        username,
+        email,
+        password,
+        address,
+        zipcode,
+        discount
+      };
+       dispatch(register(userData));
+    }
+  };
+
   return (
     <Card
       style={{
@@ -27,35 +89,82 @@ const Register = () => {
         </Typography>
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <TextField label='Username' variant='outlined' fullWidth />
+            <TextField
+              label='Username'
+              variant='outlined'
+              fullWidth
+              name='username'
+              value={username}
+              onChange={onChange}
+            />
           </Grid>
           <Grid xs={12} item>
-            <TextField label='Email' variant='outlined' fullWidth />
+            <TextField
+              label='Email'
+              variant='outlined'
+              fullWidth
+              name='email'
+              value={email}
+              onChange={onChange}
+            />
           </Grid>
           <Grid xs={12} item>
-            <TextField label='Password' variant='outlined' fullWidth />
+            <TextField
+              label='Password'
+              variant='outlined'
+              fullWidth
+              name='password'
+              value={password}
+              onChange={onChange}
+            />
           </Grid>
           <Grid xs={12} item>
-            <TextField label='Retype Password' variant='outlined' fullWidth />
+            <TextField
+              label='Retype Password'
+              variant='outlined'
+              fullWidth
+              name='password2'
+              value={password2}
+              onChange={onChange}
+            />
           </Grid>
           <Grid xs={12} item>
-            <TextField label='Address' variant='outlined' fullWidth />
+            <TextField
+              label='Address'
+              variant='outlined'
+              fullWidth
+              name='address'
+              value={address}
+              onChange={onChange}
+            />
           </Grid>
           <Grid xs={12} item>
-            <TextField label='Zip Code' variant='outlined' fullWidth />
+            <TextField
+              label='Zip Code'
+              variant='outlined'
+              fullWidth
+              name='zipcode'
+              value={zipcode}
+              onChange={onChange}
+            />
           </Grid>
         </Grid>
         <FormControlLabel
           control={<Checkbox />}
           label='I want to be a remember to receive additional discounts'
+          name='discount'
+          value={discount}
+          onChange={onChange}
         />
 
         <Box textAlign='center'>
-          <Button variant='outlined'>Sign In</Button>
+          <Button variant='outlined' onClick={onSubmit}>
+            Sign In
+          </Button>
         </Box>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
